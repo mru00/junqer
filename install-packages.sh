aa8602f0fab5ba083069f44f44b6d5c5cb9781ae
mru <mru@sisyphus.teil.cc>
2011-02-01 13:41:05 +0100

    add debug infrastructure

---
+++ install-packages.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+exec sudo apt-get install libgstreamer0.10 libgstreamer0.10-0-dbg python-gst0.10 python-gst0.10-dbg 
---
+++ print-package-info.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+exec dpkg -l python-gst0.10 python-gst0.10-dbg | cat
+
